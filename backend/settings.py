745c22032e64617e3129fb24494cbc75c697fc02
Roilin <skirills267@gmail.com>
2022-10-15 13:26:12 +0300

    3

--- backend/settings.py
+++ backend/settings.py
@@ -12,7 +12,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -22,8 +21,7 @@ SECRET_KEY = 'django-insecure-ps%*3(lvs2=sm%(rp$=m^(e0r1(0^#d)8a@&q&iuj)@a-4igu2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['http://127.0.0.1:8000/']
 
 # Application definition
 
@@ -44,12 +42,14 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'backend.urls'
@@ -72,7 +72,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
@@ -82,7 +81,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -102,7 +100,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
@@ -113,7 +110,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
@@ -126,11 +122,13 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES':[
+    'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ]
 }
 
-CORS_ORIGIN_WHITELIST = [
-  'http://localhost:3000',
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'https://transcendent-empanada-d652a0.netlify.app',
+
 ]
--- backend/wsgi.py
+++ backend/wsgi.py
@@ -5,7 +5,7 @@ path = '/home/Roilin/todoapp/'
 
 if path not in sys.path:
     sys.path.append(path)
-os.environ['DJANGO_SETTINGS_MODULE'] = 'backend.settings'
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'backend.settings'
 # then:
 from django.core.wsgi import get_wsgi_application
 
